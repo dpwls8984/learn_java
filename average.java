726a48b40eedb430e00ec7f1893f83e1592b8706
Yejin <dpwls8972@naver.com>
2021-09-18 21:01:58 +0900

    Create average.java

---
+++ average.java
@@ -0,0 +1,16 @@
+public class average {
+
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int c = 6;
+        int d = 8;
+
+        int sum = a + b + c + d;
+        int count = 4;
+
+        double average = (double)sum / (double)count;
+
+        System.out.println(average);
+    }
+}
