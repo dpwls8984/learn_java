02b121a5b67611858f2c15a5fcd52dbc4ef8b739
Yejin <dpwls8972@naver.com>
2021-09-22 16:28:57 +0900

    Update book.java

--- book.java
+++ book.java
@@ -1,11 +1,13 @@
 public class book {
     public static void main(String[] args)
     {
-        int num = 3;
-        System.out.println("변수 num의 값은 " + num + "입니다.");
-        
-        num = 5;
-        System.out.println("변수 num의 값을 변경했습니다.");
-        System.out.println("변수 num의 값은 " + num + "입니다.");
+        int num1, num2;
+        num1 = 3;
+        System.out.println("변수 num의 값은 " + num1 + "입니다.");
+
+        num2 = num1;
+
+        System.out.println("변수 num1의 값을 변수 num2에 대입했습니다.");
+        System.out.println("변수 num2의 값은 " + num2 + "입니다.");
     }
 }
