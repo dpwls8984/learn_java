98f8fa712aea2e7adb1ddfb78e61a6718ab2f832
Yejin <dpwls8972@naver.com>
2021-09-22 17:53:56 +0900

    Update book.java

--- book.java
+++ book.java
@@ -3,18 +3,15 @@ import java.io.*;
 public class book {
     public static void main(String[] args) throws IOException
     {
-       System.out.println("정수 2개를 입력하십시오.");
+       System.out.println("당신은 몇 살입니까?");
 
        BufferedReader br = 
         new BufferedReader(new InputStreamReader(System.in));
 
-        String str1 = br.readLine();
-        String str2 = br.readLine();
+        String str = br.readLine();
 
-        int num1 = Integer.parseInt(str1);
-        int num2 = Integer.parseInt(str2);
+        int num = Integer.parseInt(str);
 
-        System.out.println(num1 + "(이)가 입력되었습니다.");
-        System.out.println("그 다음으로, " + num2 + "(이)가 입력되었습니다." );
+        System.out.println("당신은 " + num + "살입니다.");
     }
 }
