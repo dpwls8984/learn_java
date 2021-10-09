ebd4dcb337318635574efd4faa261c5732b23b15
Yejin <dpwls8972@naver.com>
2021-10-09 21:44:22 +0900

    Update ex2.java

--- ex2.java
+++ ex2.java
@@ -1,10 +1,10 @@
 public class ex2 {
     public static void main(String [] args) {
-        int a = 2;
-        int b = 4;
-        int c = 6;
-        int d = 8;
-        int result = (a+b+c+d)/4;
+        double a = 2;
+        double b = 3;
+        double c = 6;
+        double d = 8;
+        double result = (a+b+c+d)/4.0;
         System.out.println(result);  
     }
 }
