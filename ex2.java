09c2aa297b75ec3621b651b3e8d0aec8f10e58ad
Yejin <dpwls8972@naver.com>
2021-10-09 20:54:53 +0900

    Create ex2.java

---
+++ ex2.java
@@ -0,0 +1,10 @@
+public class ex2 {
+    public static void main(String [] args) {
+        System.out.println("5");
+        System.out.println("4");
+        System.out.println("3");
+        System.out.println("2");
+        System.out.println("1");
+        System.out.println("미사일 발사");
+    }
+}
