ffb0b9020b04f82f623dce5a9cdc957e52e0eb59
Yejin <dpwls8972@naver.com>
2021-10-09 21:01:34 +0900

    Update ex2.java

--- ex2.java
+++ ex2.java
@@ -1,10 +1,10 @@
 public class ex2 {
     public static void main(String [] args) {
-        System.out.println("5");
-        System.out.println("4");
-        System.out.println("3");
-        System.out.println("2");
-        System.out.println("1");
-        System.out.println("미사일 발사");
+        int a = 2;
+        int b = 4;
+        int c = 6;
+        int d = 8;
+        int result = (a+b+c+d)/4;
+        System.out.println(result);  
     }
 }
