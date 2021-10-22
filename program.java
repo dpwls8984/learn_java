c96c886435ffe3ab9394c06a93443c5be4468211
Yejin <dpwls8972@naver.com>
2021-10-22 09:39:30 +0900

    Update program.java

--- program.java
+++ program.java
@@ -1,11 +1,6 @@
 public class program {
 
     public static void main(String[] args) {
-       int a = 2;
-       int b = 3;
-       int c = 6;
-       int d = 8;
-       int result = (a+b+c+d)/4;
-       System.out.println(result);   
+        
     }
 }
