f3ba32a475f76113315cac50ebee2e0a87f88d8f
Yejin <dpwls8972@naver.com>
2021-12-07 23:05:34 +0900

    Update program.java

--- program.java
+++ program.java
@@ -1,18 +1,16 @@
+import java.util.Scanner;
+
 public class program {
 
     public static void main(String[] args) {
-        //주사위의 합이 4의 배수인 경우 출력
-        int sum = 0;
-        for(int i = 1;i<7;i++)
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        
+        for(int j = 0;j<num;j++)
         {
-            for(int j = 1;j<7;j++)
-            {
-                sum = i + j;
-                if(sum % 4 == 0)
-                {
-                    System.out.println(i + ", " + j);
-                }
-            }
+            for(int i = 0;i<j+1;i++)
+                System.out.print("*");
+            System.out.println();
         }
     }
 }
